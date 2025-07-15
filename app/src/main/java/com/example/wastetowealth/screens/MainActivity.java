939922017fc97ad8553aa9c0c80d0a9727ea909a
ersurajrajput <thsurajsinghrajput@gmail.com>
2025-07-15 18:17:29 +0530

    navigation bar added

--- app/src/main/java/com/example/wastetowealth/screens/MainActivity.java
+++ app/src/main/java/com/example/wastetowealth/screens/MainActivity.java
@@ -1,16 +1,30 @@
 package com.example.wastetowealth.screens;
+import static android.widget.Toast.LENGTH_SHORT;
+
+import com.example.wastetowealth.R;
+
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.wastetowealth.R;
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    ConstraintLayout profilePanel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +36,32 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+
+                if (menuItem.getItemId() == R.id.home){
+                    Toast.makeText(getApplicationContext(),"Home",LENGTH_SHORT).show();
+                    return true;
+                }else if (menuItem.getItemId() == R.id.cart){
+                    Toast.makeText(getApplicationContext(),"Cart",LENGTH_SHORT).show();
+                    return true;
+                }if (menuItem.getItemId() == R.id.search){
+                    Toast.makeText(getApplicationContext(),"Search",LENGTH_SHORT).show();
+                    return true;
+                }if (menuItem.getItemId() == R.id.history){
+                    Toast.makeText(getApplicationContext(),"History",LENGTH_SHORT).show();
+                    return true;
+                }if (menuItem.getItemId() == R.id.profile){
+                    Toast.makeText(getApplicationContext(),"Profile",LENGTH_SHORT).show();
+                    return true;
+                }
+                return false;
+            }
+        });
+
     }
 }
