fa01f5d5f097c25d1934b98e73bfb800dd529013
ShivangSaxena05 <82094331+ShivangSaxena05@users.noreply.github.com>
2025-07-18 11:44:49 +0530

    Commiting the button in adbout us

--- app/src/main/java/com/example/wastetowealth/screens/AboutUsActivity.java
+++ app/src/main/java/com/example/wastetowealth/screens/AboutUsActivity.java
@@ -3,6 +3,8 @@ package com.example.wastetowealth.screens;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,10 +16,13 @@ import com.example.wastetowealth.R;
 import com.example.wastetowealth.StartScreen;
 
 public class AboutUsActivity extends AppCompatActivity {
+    Button aboutUsBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_about_us);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -25,6 +30,7 @@ public class AboutUsActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        aboutUsBack = findViewById(R.id.aboutUsBack);
         SharedPreferences sharedPref = getSharedPreferences("UserSession", MODE_PRIVATE);
         boolean isLoggedIn = sharedPref.getBoolean("isLoggedIn", false);
 
@@ -34,5 +40,13 @@ public class AboutUsActivity extends AppCompatActivity {
             finish();
             // redirect to home/dashboard
         }
+        aboutUsBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Homepage = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(Homepage);
+                finish();
+            }
+        });
     }
 }
